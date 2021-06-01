afddc74bb10b49d4a784ec4a3be0a0a942e43fee
carlosam2 <a01703682@itesm.mx>
2021-06-01 13:51:06 -0500

    Initial Commit

---
+++ LiveMovies/LiveMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveMoviesApp.swift
+//  LiveMovies
+//
+//  Created by Carlos Ayala Medina on 01/06/21.
+//
+
+import SwiftUI
+
+@main
+struct LiveMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
